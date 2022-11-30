a72532757bba302b7a303a001c134c29b03cde39
vanessaspi <114577439+vanessaspigariol@users.noreply.github.com>
2022-11-30 13:12:50 +0000

    4 finale

--- esl4.py
+++ esl4.py
@@ -1,19 +1,19 @@
 class CSVfile():
-    
     def __init__(self, name):
         self.name=name
-    csvfile=CSVfile(shampoo_sales.csv)
-    csvfile=open('shampoo_sales.csv', 'r')
-    print(csvfile.name)
-    
-    def get_data(self):
-        elements=[]
-        my_file=open(self.name, 'r')
-        for line in my_file:
-            if line != 'Date, Sales\n':
-                elements.append(line)
-        my_file.close()
 
-    csv_file = CSVFile("shampoo_sales.csv") #istanzi l'oggetto csv_file, passandogli come argomento il nome del file
-    data = csv_file.get_data() # chiami il metodo get_data dell'oggetto e salvi l'output nella variabile data
-    print(data) # infine puoi stampare il contenuto della variabile per controllare cosa fa il metodo get_data
+    def get_data(self):
+        my_file=open(self.name, 'r')
+        data=[]
+        for line in my_file:
+            if line == 'Date,Sales\n':
+                elements=None
+            else:
+                elements=line.split(',')
+                data.append(elements)
+        
+        my_file.close()
+        print (data)
+
+my_file=CSVfile("shampoo_sales.csv")
+my_file.get_data()
