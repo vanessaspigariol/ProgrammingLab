05eea176051314fd22efcbb3b53202e8778f6da0
vanessaspi <114577439+vanessaspigariol@users.noreply.github.com>
2023-02-21 18:42:03 +0000

    .

--- esame/esame.py
+++ esame/esame.py
@@ -14,7 +14,7 @@ class CSVFile():
     def get_data(self):
         if not self.can_read:
             print('errore, file non aperto o leggibile')
-            return None # eventuale fine
+            return None #eventuale fine
         else:
             data=[] #lista vuota per salvarci i dati
             my_file=open(self.name,'r') #riapro il file per non saltare la prima riga
@@ -35,22 +35,19 @@ class CSVTimeSeriesFile(CSVFile):
         for item in string_data:
             if item is not list:
                 raise ExamException('errore, elemento lista non è una lista: manca uno dei due valori')
-            if len(item)!=2:
-                raise ExamException('errore, lista non contiene due valori')
+            if len(item)<=2:
+                raise ExamException('errore, lista non contiene due valori') #la lista deve avere almeno due valori (data e numero passeggeri)
             date=item[0].split('-') #splitto il primo elemento dela lista per separare anno e mese
             try:
-                year=int(date[0])
-                month=int(date[1])
+                date[0]=int(date[0])
+                date[1]=int(date[1])
             except ValueError:
                 raise ExamException('errore, anno o meso sono valori non interi')
             try:
-                int(item[1])
+                item[1]=int(item[1])
             except ValueError:
                 raise ExamException('errore, numero passeggeri non intero')
-            if int(year)<1949 or int(year)>1960 or int(month)<=0 or int(month)>12:
-                raise ExamException('errore, data non valida')
-            else:
-                numerical_data.append(item) #aggiungo la lista contenente data e numero passeggeri dopo aver controllato la validità del dato
+            numerical_data.append(item) #aggiungo la lista contenente data e numero passeggeri dopo aver controllato la validità del dato
             return numerical_data
             
 time_series_file=CSVTimeSeriesFile(name='data.csv')
@@ -73,8 +70,6 @@ def detect_similar_monthly_variations(time_series,years):
         anno2=int(years[1])
     except Exception:
         raise ExamException('errore, secondo anno non intero')
-    if int(anno1)<1949 or int(anno1)>1960 or int(anno2)<1949 or int(anno2)>1960:
-        raise ExamException('errore, anni non validi')
     if int(anno1)-int(anno2)!=1 and int(anno1)-int(anno2)!=-1:
         raise ExamException('errore, anni non consecutivi')
     prima_lista=[] #lista elementi primo anno
