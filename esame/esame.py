cc70f9d06b7ffac40f5927241804290293c7e884
vanessaspigariol <114577439+vanessaspigariol@users.noreply.github.com>
2023-06-19 11:58:53 +0000

    !

--- esame/esame.py
+++ esame/esame.py
@@ -1,230 +1,277 @@
 class ExamException(Exception):
     pass
 
-class CSVFile():
-    def __init__(self, name):
-        self.name = name  #inizializzo nome file
-        self.can_read = True  #provo ad aprire file e leggerlo
+class CSVTimeSeriesFile():
+    
+    def __init__(self,name):
+        #inizializzo nome
+        self.name=name 
+            
+    def get_data(self):
+        
+        ##CONTROLLO NOME
+        if self.name is None:
+            raise ExamException('Errore: nome file non inserito in input!')
+
+        #provo ad aprire il file
         try:
-            my_file = open(self.name, 'r')
+            my_file=open(self.name, 'r')
             my_file.readline()
-        except ExamException as e:
-            self.can_read = False
-            print('errore, file non leggibile: {}'.format(e))
-
-    def get_data(self):
-        if not self.can_read:
-            print('errore, file non aperto o leggibile')
-            return None  #eventuale fine
-        else:
-            data = []  #lista vuota per salvarci i dati
-            my_file = open(self.name,
-                           'r')  #riapro il file per non saltare la prima riga
-            for line in my_file:
-                elements = line.split(',')
-                elements[-1] = elements[-1].strip(
-                )  #pulisco il carattere da spazi bianchi
-                if elements[0] != 'date':
-                    data.append(
-                        elements
-                    )  #aggiungo elementi alla lista escludendo l'intestazione
+            #chiudo il file
             my_file.close()
-            return data  #ritorno i dati salvati dopo aver processato tutto
+        except Exception:
+            raise ExamException('Errore: non è stato possibile aprire o leggere il file!')
 
+        ##SALVATAGGIO DATI VALIDI
+        lista=[]
+        
+        #riapro il file
+        my_file=open(self.name, 'r')
 
-class CSVTimeSeriesFile(CSVFile):
-    def get_data(self):
-        #chiamo metodo get_data di CSVFile e alzo eccezioni nel caso il file non esista o non sia leggibile
-        string_data = super().get_data()
-        if string_data == []:
-            raise ExamException('errore, lista csv file vuota')
-        if string_data is None:
-            raise ExamException(
-                'errore, il metodo get_data di csvfile è nullo')
+        #leggo il file linea per linea
+        for line in my_file:
+            
+            #pulizia carattere dall'ultimo elemento e split sulla virgola
+            elements=line.strip("\n").split(",")
+            
+            #elements[0]=data anno-mese
+            #elements[1]=passeggeri
 
-        #creo una lista che possa contenere i valori validi
-        numerical_data = []
-        for item in string_data:
+            #se NON sto processando l'intestazione
+            if elements[0].strip() == 'date':
+                continue
+
+            #variabile ausiliara per fare un check su anni e mesi
+            tempo=elements[0].split('-') 
+            
+            #tempo[0]=anni
+            #tempo[1]=mesi
+            #elements[1]=passeggeri
+
+            try:
+                tempo[0] = int(tempo[0])
+                tempo[1] = int(tempo[1])
+                elements[1]=int(elements[1])
+            except Exception:
+                continue
+            
+                            
+            #valori negativi
+            if int(elements[1])<=0:
+                continue
+
+            lista.append(elements)
+        
+        #chiudo il file
+        my_file.close()
+
+        ##CONTROLLO ORDINE E DUPLICAZIONE
+        numerical_data=[]
+        
+        for item in lista:
             if item == []:
                 continue
-            if item is not list:
-                continue
-            if len(item) < 2:
-                continue  #la lista deve avere almeno due valori (data e numero passeggeri)
 
-            #controllo che i valori siano interi
+            #la lista deve avere almeno due valori (data e numero passeggeri)
+            if len(item) < 2:
+                continue  
+
+            #trasformo i dati in intero e assegno il valore a delle variabili più chiare, utili per il prossimo controllo
             date = item[0].split('-')  #separo anno e mese
             try:
-                anno = int(date[0])  #anno è un intero
-                mese = int(date[1])  #mese è un intero
+                anno = int(date[0])  
+                mese = int(date[1])  
+                passeggero = int(item[1])
             except Exception:
-                pass
-            try:
-                passeggero = int(item[1])  #passeggero è un intero
-            except Exception:
-                pass
+                continue
+                        
 
-            #controllo che i dati siano ordinatiper aggungerli a numerical_data
-            if item == string_data[0]:
+            #controllo che i dati siano ordinati per aggungerli a numerical_data
+            #altrimenti alzo un'eccezione
+            if item == lista[0]:
                 prev_anno = anno
                 prev_mese = mese
             else:
                 if anno < prev_anno:
-                    raise ExamException('errore, anni non ordinati')
-                if mese <= prev_mese and anno == prev_anno:
-                    raise ExamException(
-                        'errore, mesi non ordinati o duplicati')
+                    raise ExamException('Errore, anni non ordinati!')
+                    continue
+                if anno == prev_anno and mese <= prev_mese:
+                    raise ExamException('Errore, mesi non ordinati o duplicati!')
+                    continue
                 prev_anno = anno
                 prev_mese = mese
-            if anno > 0 and mese > 0 and mese <= 12 and passeggero >= 0:
+            if anno > 0 and mese > 0 and mese <= 12 and passeggero > 0:
                 numerical_data.append(item)
+
         return numerical_data
-
-
-time_series_file = CSVTimeSeriesFile(name='data.csv')
-time_series = time_series_file.get_data()
-
-
-def detect_similar_monthly_variations(time_series, years):
-    #controllo liste in input
-    if time_series == []:
-        raise ExamException('errore, lista time_series vuota')
-
-    if years == []:
-        raise ExamException('errore, lista anni vuota')
-    if len(years) != 2:
-        raise ExamException('errore, lista anni ha una lunghezza non valida')
-
-    #distinguo e controllo i due anni della lista years
-    try:
-        anno1 = int(years[0])
-    except Exception:
-        raise ExamException('errore, primo anno non intero')
-    try:
-        anno2 = int(years[1])
-    except Exception:
-        raise ExamException('errore, secondo anno non intero')
-    if anno1 < 0 or anno2 < 0:
-        raise ExamException('errore, anni non esistenti')
-    if anno1 - anno2 != 1 and anno1 - anno2 != -1:
-        raise ExamException('errore, anni non consecutivi')
-
-    #creo una lista per i valori ciascun anno
-    prima_lista = []
-    existence1 = False  #sarà utile per determinare se il primo anno è presente tra i dati
-    for element in time_series:
-        #trovo tutti i dati corrispondendi ad anno1
-        tempo = element[0].split('-')  #divido anno(tempo[0]) e mese(tempo[1])
-        if tempo[0] == anno1:
-            existence1 = True
-            mese = (tempo[1]) - 1
-            try:
-                #inserisco il numero dei passeggeri(element[1]) nella posizione corrispondente al mese in cui sono stati rilevati
-                prima_lista.insert(mese, int(element[1]))
-            except:
-                raise ExamException(
-                    'errore, non è stato possibile aggiungere un valore alla lista del primo anno'
-                )
-    if existence1 is False:
-        raise ExamException('errore, primo anno di years non valido')
-
-    seconda_lista = []  #lista elementi secondo anno
-    existence2 = False  #sarà utile per determinare se il primo anno è presente tra i dati
-    for element in time_series:
-        #trovo tutti i dati corrispondendi ad anno2
-        tempo = element[0].split('-')  #divido anno(tempo[0]) e mese(tempo[1])
-        if tempo[0] == anno2:
-            existence2 = True
-            mese = (tempo[1]) - 1
-            try:
-                #inserisco il numero dei passeggeri (element[1]) nella posizione corrispondente al mese in cui sono stati rilevati i dati
-                seconda_lista.insert(mese, int(element[1]))
-            except:
-                raise ExamException(
-                    'errore, non è stato possibile aggiungere un valore alla lista del secondo anno'
-                )
-    if existence2 is False:
-        raise ExamException('errore, secondo anno di years non valido')
-
-    #controllo le due liste ottenute e controllo che abbiano 12 elementi, anche nulli
-    if prima_lista == []:
-        raise ExamException('errore, lista anno1 vuota')
-    if prima_lista is not list:
-        raise ExamException('errore, lista anno1 non è una lista')
-    if len(prima_lista) != 12:
-        for i in enumerate(11):
-            if prima_lista[i] is not None:
-                pass
-            else:
-                prima_lista[i] = None
-    if seconda_lista == []:
-        raise ExamException('errore, lista anno2 vuota')
-    if seconda_lista is not list:
-        raise ExamException('errore, lista anno2 non è una lista')
-    if len(seconda_lista) != 12:
-        for i in enumerate(11):
-            if seconda_lista[i] is not None:
-                pass
-            else:
-                seconda_lista[i] = None
-
-    #creo liste per salvare le variazioni tra mesi
-    variazione1 = []
-    for i in prima_lista:
-        if i == prima_lista[0]:
-            prev_value = i  #gennaio non ha mesi precedenti, quindi non calcolo la differenza
-        elif i == None:
-            prev_value = i
-            variazione1.append(None)
-        elif prev_value == None:
-            variazione1.append(None)
-            prev_value = i
-        else:
-            differenza = int(i - prev_value)
-            prev_value = i
-            variazione1.append(int(differenza))
-    variazione2 = []  #lista con variazioni tra valori seconda lista
-    for i in seconda_lista:
-        if i == seconda_lista[0]:
-            prev_value = i  #gennaio non ha mesi precedenti, quindi non calcolo la differenza
-        elif i == None:
-            prev_value = i
-            variazione2.append(None)
-        elif prev_value == None:
-            variazione2.append(None)
-            prev_value = i
-        else:
-            differenza = int(i - prev_value)
-            prev_value = i
-            variazione2.append(int(differenza))
-            
-    if variazione1 == []:
-        raise ExamException('errore, variazione1 non ha valori numerici')
-    if len(variazione1) != 11:
-        raise ExamException('errore, lista variazione1 non ha 11 elementi')
-    if variazione1 == None:
-        raise ExamException('errore, variazione1 non ha valori')
         
-    if variazione2 == []:
-        raise ExamException('errore, variazione2 non ha valori numerici')
-    if len(variazione2) != 11:
-        raise ExamException('errore, lista variazione2 non ha 11 elementi')
-    if variazione2 == None:
-        raise ExamException('errore, variazione2 non ha valori')
 
-    #lista finale confronterà parallelamente le variazioni delle due liste ritornando vero o falso
-    lista_finale = []
-    for i in enumerate(10):
-        if variazione1[i] is None or variazione2[i] is None:
-            lista_finale.append(False)
-        else:
-            differenza = variazione1[i] - variazione2[
-                i]  #differenze tra le stesse coppie di mesi in anni consecutivi
-        if differenza >= -2 and differenza <= 2:
-            lista_finale.append(True)  #simili
-        else:
-            lista_finale.append(False)  #non simili
-    return lista_finale
 
-#years=[1949,1950]
-#print(detect_similar_monthly_variations(time_series, years))
+#time_series_file = CSVTimeSeriesFile(name='data.csv')
+#time_series = time_series_file.get_data()
+#print(time_series)
+
+
+
+def compute_avg_monthly_difference(time_series, first_year, last_year):
+
+    ##CONTROLLI DATI INPUT
+    if not time_series:
+        raise ExamException('Errore, lista time_series vuota!')
+    
+    try:
+        anno_inizio = int(first_year)
+        anno_fine = int(last_year)
+    except Exception:
+        raise ExamException('Errore, non è stato possibile convertire gli anni in input in interi!')
+
+    if anno_inizio > anno_fine:
+        raise ExamException('Errore, anno inizio maggiore di anno fine!')
+        
+    #anno inizio nella lista
+    found_inizio = False 
+    for el in time_series:
+        #variabile temporanea per capire se gli anni in input sono presenti nella lista
+        tempAnno = int(el[0].split("-")[0])
+        if  tempAnno == anno_inizio:
+            #se trovo l'anno nella lista allora esco dal ciclo
+            found_inizio = True 
+            break
+
+    if not found_inizio:
+        raise ExamException('Errore, anno inizio non nella lista!')
+
+    #anno fine nella lista
+    found_fine = False
+    for el in time_series:
+        #variabile temporanea per capire se gli anni in input sono presenti nella lista 
+        tempAnno = int(el[0].split("-")[0])
+        if  tempAnno == anno_fine:
+            #se trovo l'anno nella lista allora esco dal ciclo
+            found_fine = True 
+            break
+
+    if not found_fine:
+        raise ExamException('Errore! Anno fine non nella lista!')
+        
+
+    
+    ##INIZIO PRIMA PARTE
+    #creo una lista('listaanni') che conterrà un numero di liste pari al numero di anni presi in esame
+    #ogni sotto-lista('annata') conterrà il numero di passeggeri nella posizione corrispondente al mese in cui sono stati rilevati
+    listaanni=[]
+
+    #inizializzo il primo anno da considerare
+    prev_anno=anno_inizio
+
+    #riempio con 12 zeri la lista che userò per il primo anno
+    annata=[]
+    for i in range(12):
+        annata.append(0)
+
+    #nel ciclo for sostituisco gli zeri con il numero di passeggeri nella posizione corrispondente al mese-1 in cui sono stati rilevati, se sono stati rilevati
+    for element in time_series:
+        tempo = element[0].split('-')
+        try:
+            anno = int(tempo[0])
+            mese = int(tempo[1])-1
+            passeggero = int(element[1])
+        except Exception:
+            continue
+        
+        #restiamo nell'arco di tempo determinato dagli anni presi in input
+        #anno>anno_fine+1 per non escludere l'estremo
+        if anno < anno_inizio or anno > anno_fine:
+            continue
+        
+        #casi da considerare:
+        
+        #1:sono al primo anno o ad un anno già "salvato"(dopo il caso #2)
+        if anno==prev_anno:
+            annata[mese]=passeggero
+            
+        #2:anno diverso dal precedente
+        #assumiamo di avere almeno una misurazione all’anno
+        #quindi se anno!=prev_anno, anno==prev_anno+1
+        #->inizializzo il nuovo anno a prev_anno
+        #->salvo la lista appena creata e ne creo una nuova per il nuovo anno
+            
+        elif anno==prev_anno+1:
+            prev_anno=anno
+            listaanni.append(annata)
+            annata=[]
+            for i in range(12):
+                annata.append(0)
+            annata[mese]=passeggero
+        
+    listaanni.append(annata)
+    #print(listaanni)
+
+    ##FINE PRIMA PARTE:
+    #lista 'listaanni' di 12 elementi per ogni anno, dove l’elemento all’indice i corrisponde al numero di passeggeri per il mese i+1 in quell’anno
+    
+
+    ##INIZIO SECONDA PARTE
+    #ora posso iniziare a calcolare le medie, che saranno salvate nella lista 'medie'
+    medie=[]
+    
+    #conto quante liste sono presenti nella lista 'totale'
+    numero_anni=len(listaanni)
+    divisore=numero_anni-1
+    
+    #ciclo for sui 12 mesi
+    #counter salva l'indice delle sotto-liste
+    for i in range(12):
+        som=0 
+        media=0
+        counter=numero_anni-1        
+        
+        #eslcudo la prima sotto-lista perchè non ha valori precedenti da sottrarre
+        while counter>0:
+            
+            #elemento in posizione 'i' della sotto-lista numero 'counter' o 'counter-1' presente nella lista 'totale'            
+            try:
+                primo=int(listaanni[counter][i])
+                secondo=int(listaanni[counter-1][i])
+            except Exception:
+                #se non riesco a trasformare in intero dei dati, vado avanti e riduco di uno il divisore
+                counter-=1
+                divisore-=1
+                continue #continua con la prossima iterazione del ciclo while
+
+            #intervallo di due anni, per il mese con la misurazione mancante verrà tornato come valore finale 0
+            if numero_anni==2 and (primo==0 or secondo==0):
+                media=0
+                break #interruzione ciclo while
+                
+            #intervallo di più di due anni, calcoliamo la differenza media tra le misurazioni di quel mese per gli altri anni, ignorando la misurazione mancante            
+            elif numero_anni>2:
+                if secondo==0:
+                    counter-=1
+                    continue #continua con la prossima iterazione del ciclo while
+                elif primo==0:
+                    #poichè 'secondo' diventerà 'primo',
+                    #'divisore' cambia solo ora altrimenti si ridurrebbe due volte per uno stesso 0
+                    divisore-=1
+                    counter-=1
+                    continue #continua con la prossima iterazione del ciclo while
+            diff=primo-secondo
+            som+=diff
+            counter-=1
+
+            #intervallo di più di due anni e per un mese abbiamo meno di due misurazioni, verrà tornato 0 come valore finale per quel mese
+        if numero_anni>2 and divisore<2:
+            media=0
+        
+        else:
+            media=som/divisore
+                
+        medie.append(media)   
+        
+    ##FINE SECONDA PARTE
+    return medie
+    
+
+
+#aa=compute_avg_monthly_difference(time_series, "1949", "1951")
+#print(aa)
